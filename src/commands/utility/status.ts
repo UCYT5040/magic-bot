fad484330e56ccb7c6461112ddadc9d7a677a64d
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-06-26 10:14:47 -0500

    Basic status command

---
+++ src/commands/utility/status.ts
@@ -0,0 +1,30 @@
+import {SlashCommandBuilder} from "discord.js";
+import {embed} from "../../embed";
+
+const statusData = {
+    ping: ['Ping', 'ms'],
+    uptime: ['Uptime', 's'],
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('status')
+        .setDescription('Returns various status information about the bot.'),
+    async execute(interaction) {
+        console.log('Pong!')
+        const ping = interaction.client.ws.ping
+        const data = {
+            ping: ping == -1 ? '(unknown)' : ping,
+            uptime: Math.floor(interaction.client.uptime / 1000),
+        }
+        let messageBody = '';
+        for (const [key, value] of Object.entries(statusData)) {
+            const [label, unit] = value;
+            messageBody += `**${label}:** ${data[key]} ${unit}\n`;
+        }
+        const statusEmbed = embed().setTitle('Status Information').setDescription(messageBody);
+        await interaction.reply({
+            embeds: [statusEmbed]
+        });
+    },
+};
