3dd6d195f5bf6171a17717e0eee5513312e00a5e
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-06-26 11:11:38 -0500

    Embed builder utility

---
+++ src/embed.ts
@@ -0,0 +1,7 @@
+import {EmbedBuilder} from "discord.js";
+
+export function embed() {
+    return new EmbedBuilder()
+        .setColor('#ff0040') // TODO: Make configurable
+        .setTimestamp();
+}
