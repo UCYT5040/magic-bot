a46bca115e76ea5234a83ac7c1965d20d2d39382
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-06-28 11:31:08 -0500

    Use double quotes for consistency

--- src/embed.ts
+++ src/embed.ts
@@ -2,6 +2,6 @@ import {EmbedBuilder} from "discord.js";
 
 export function embed() {
     return new EmbedBuilder()
-        .setColor('#ff0040') // TODO: Make configurable
+        .setColor("#ff0040") // TODO: Make configurable
         .setTimestamp();
 }
