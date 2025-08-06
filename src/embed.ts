c35ae60e6afd431eb519a9c76d211cb1bff56843
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2025-08-06 06:53:50 -0500

    Improve Prisma import

--- src/embed.ts
+++ src/embed.ts
@@ -1,5 +1,5 @@
 import {EmbedBuilder} from "discord.js";
-import {PrismaClient} from "../generated/prisma/client";
+import {PrismaClient} from "@prisma/client";
 
 const prisma = new PrismaClient();
 
